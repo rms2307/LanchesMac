61e96aefcf577c1a645e19e2417b03607a13f7d0
Robson Silva <rms2307@outlook.com>
2021-08-10 17:44:12 -0300

    Added: AppDBContext

---
+++ LanchesMac/Context/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        { }
+
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,6 +1,8 @@
+using LanchesMac.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,13 @@ namespace LanchesMac
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("Dev");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
+
+            services.AddDbContext<AppDBContext>(dbContextOptions =>
+                dbContextOptions.UseMySql(connectionString, serverVersion, builder =>
+                   builder.MigrationsAssembly("Dev")));
+
             services.AddControllersWithViews();
         }
 
