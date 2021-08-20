70620e5d0a983fa428180680d9270d2120239996
Robson Silva <rms2307@outlook.com>
2021-08-20 10:28:27 -0300

    Added: Migrations

--- LanchesMac/Migrations/20210820132507_Pedidos_PedidosDetalhes.Designer.cs
+++ LanchesMac/Migrations/20210820132507_Pedidos_PedidosDetalhes.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using LanchesMac.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LanchesMac.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210820132507_Pedidos_PedidosDetalhes")]
+    partial class Pedidos_PedidosDetalhes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
