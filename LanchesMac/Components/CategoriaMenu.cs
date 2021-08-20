47f2a8f1ca06521fb950b2aded28d7b99daba138
Robson Silva <rms2307@outlook.com>
2021-08-20 07:27:22 -0300

    Added: Menu categorias

---
+++ LanchesMac/Components/CategoriaMenu.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias
+                .OrderBy(c => c.CategoriaNome);
+            Console.WriteLine("TESTE");
+            return View(categorias);
+        }
+    }
+}
